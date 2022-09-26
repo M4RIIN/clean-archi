c749b3347441a5cf5cfbbe795176c03ea89cb9c5
Nicolas Marin <marin.nicolas.ludovic@gmail.com>
2022-09-26 17:02:29 +0200

    Change gui

--- consoleApp/console-app/src/main/java/com/lagrange/ApplicationConsole.java
+++ consoleApp/console-app/src/main/java/com/lagrange/ApplicationConsole.java
@@ -4,27 +4,52 @@ package com.lagrange;
 import com.lagrange.usecase.model.user.UserDto;
 import com.lagrange.usecase.model.user.colocation.Colocation;
 
+import java.util.Scanner;
+
 public class ApplicationConsole {
     public static void main(String[] args) {
         Configuration configuration = Configuration.getConfiguration();
-        insertUser( "marin1235", "Marin1");
-        insertUser( "Marty", "McflyMarty12");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("CU - Create user");
+            System.out.println("LU - List user ");
+            System.out.println("CC - Create colocation ");
+            System.out.println("LC - List Colocation ");
 
-        insertColocation("colocation", "password", "1234");
-        insertColocation("Coloc de Fou", "password", "8521");
-        displayAllUser();
-        displayAllColocation();
+            String entreeUser = scanner.nextLine();
+            switch (entreeUser){
+                case "CU":
+                    System.out.println("<pseudo>;<password>");
+                    String secondEntreeUser = scanner.nextLine();
+                    insertUser(secondEntreeUser.split(";")[1],secondEntreeUser.split(";")[0]);
+                    break;
+                case "LU":
+                    displayAllUser();
+                    break;
+                case "CC":
+                    System.out.println("<pseudo>;<password>;<tag>");
+                    String secondEntreeColoc = scanner.nextLine();
+                    insertColocation(secondEntreeColoc.split(";")[0],secondEntreeColoc.split(";")[1],secondEntreeColoc.split(";")[2]);
+                    break;
+                case "LC":
+                    displayAllColocation();
+                    break;
+                default:
+                    System.out.println("Commande inconnue.");
+                    break;
+            }
+        }
     }
 
-    private static void displayAllColocation() {
+    private static void displayAllUser() {
         for(UserDto userDto : Configuration.getConfiguration().listAllUserService.listAll()){
             System.out.println(userDto.getPseudo() + " - " + userDto.getPassword());
         }
     }
 
-    private static void displayAllUser() {
+    private static void displayAllColocation() {
         for(Colocation coloc : Configuration.getConfiguration().listAllColocationBoundary.listAll()){
-            System.out.println(coloc.getPseudo() + "-" + coloc.getPassword() + " -  #" + coloc.getTag());
+            System.out.println(coloc.getPseudo() + " - " + coloc.getPassword() + " - #" + coloc.getTag());
         }
     }
 
